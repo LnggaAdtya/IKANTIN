731cc518c772f7a6a7570bcd640653722c787cbf
LnggaAdtya <linggaaditya@smkwikrama.sch.id>
2022-08-01 13:55:51 +0700

    woi

---
+++ fungsi.php
@@ -0,0 +1,51 @@
+<?php
+class Produk {
+    public $bakwan,
+            $bakso,
+            $jmlBakwan,
+            $jmlBakso,
+            $hargaBakwan,
+            $hargaBakso,
+            $totalSeluruh,
+            $totalHargaBakwan,
+            $totalHargaBakso;
+    
+    function __construct() {
+        $this->hargaBakwan = 2000;
+        $this->hargaBakso = 10000;
+    }
+}
+
+class Jumlah extends Produk {
+    public function getJumlah($jmlBakwan, $jmlBakso){
+        $this->jmlBakwan = $jmlBakwan;
+        $this->jmlBakso = $jmlBakso;
+    }
+
+    public function setHarga(){
+        $this->totalHargaBakwan = $this->hargaBakwan *  $this->jmlBakwan;
+        $this->totalHargaBakso = (int)$this->hargaBakso *  (int)$this->jmlBakso;
+        $this->totalSeluruh = $this->totalHargaBakwan + $this->totalHargaBakso;
+    }
+
+    public function cetakStruk(){
+        echo "** <b>$ iKantin Wikrama $</b> **";
+        echo "<br>";
+        echo "Bakwan : $this->jmlBakwan x Rp. $this->hargaBakwan : <b> $this->totalHargaBakwan</b>";
+        echo "<br>";
+        echo "Bakso : $this->jmlBakso x Rp. $this->hargaBakso : <b> $this->totalHargaBakso</b>";
+        echo "<br><br>";
+        echo "Total Bayar : Rp. <b>$this->totalSeluruh</b> ";
+        echo "<br>";
+        echo "***************";
+    }
+}
+
+
+
+
+
+
+
+
+?>
